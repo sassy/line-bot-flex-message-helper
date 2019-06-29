28badadfe4834263aef2920f8d9fa23700e1e393
sassy <sassy.watanabe@gmail.com>
2019-06-29 19:14:58 +0900

    add hello, world case.

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,68 @@
-export type ContainerType = 'bubble' | 'carousel'
+export type ContainerType = 'bubble' | 'carousel';
+export type BoxLayout = 'horizontal' | 'vertical' | 'baseline';
+
+
+interface ContentBase {
+    type: string;
+}
+
+export interface TextContent extends ContentBase {
+    text: string,
+    size?: string,
+    align?: string,
+    color?: string,
+}
+
+export class Box {
+    json:any;
+
+    constructor(boxLayout: BoxLayout) {
+        this.json = {
+            type: "box",
+            layout: boxLayout,
+            contents: []
+        };
+    }
+
+    public addContent(content: TextContent): this {
+        this.json['contents'].push(content);
+        return this;
+    }
+}
+
+interface ContainerBase {
+    json: any
+}
+
+export class BubbleConteiner implements ContainerBase {
+    public json: any;
+    constructor() {
+        this.json = {
+            type: 'bubble'
+        }
+    }
+
+    public setHeaderBlock() {
+        // TODO
+    }
+
+    public setHeroBlock() {
+        // TODO
+    }
+
+    public setBodyBlock(box: Box): this {
+        this.json['body'] = box.json;
+        return this;
+    }
+
+    public setFooterBlock() {
+        //TODO
+    }
+}
+
+class CarouselContainer implements ContainerBase {
+    public json: any;
+}
 
 class Helper {
     json: any;
@@ -11,14 +75,14 @@ class Helper {
         return JSON.stringify(this.json);
     }
 
-    public setContainerType(type: ContainerType): this {
-        this.json['type'] = type;
+    public setContainerType(container: BubbleConteiner | CarouselContainer): this {
+        this.json = container.json;
         return this;
     }
 
 }
 
-export default class Client {
+export class Client {
     public static Helper(): Helper {
         return new Helper();
     }
--- test/client.test.ts
+++ test/client.test.ts
@@ -1,8 +1,23 @@
-import Client from '../src/client';
+import { Client, Box, BubbleConteiner} from '../src/client';
 
 describe('test', () => {
     it ('test', () => {
-        const ret = Client.Helper().setContainerType('bubble').dump();
-        expect(ret).toEqual('{"type":"bubble"}');
+        const container = new BubbleConteiner();
+        const textBox = new Box('vertical').addContent({
+            type: 'text',
+            text: 'Hello,World'
+        });
+        container.setBodyBlock(textBox)
+
+        const ret = Client.Helper().setContainerType(container).dump();
+        expect(ret).toEqual(
+            '{"type":"bubble",'
+            + '"body":{'
+            + '"type":"box",'
+            + '"layout":"vertical",'
+            + '"contents":[{"type":"text","text":"Hello,World"}]}}'
+            );
     })
+
+
 });
