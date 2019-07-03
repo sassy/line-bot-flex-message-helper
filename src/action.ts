22c113d30d129b541cc8e297b4a2ad05ff250491
sassy <sassy.watanabe@gmail.com>
2019-07-03 23:28:01 +0900

    add actions.

--- src/action.ts
+++ src/action.ts
@@ -27,3 +27,110 @@ export class PostbackAction {
         return JSON.stringify(this.json);
     }
 }
+
+type MessageJson = {
+    type: string;
+    label: string;
+    text: string;
+}
+
+export class MessageAction {
+    json: MessageJson;
+
+    constructor(label:string, text: string) {
+        this.json = {
+            type: 'message',
+            label: label,
+            text: text,
+        }
+    }
+
+    public dump(): string {
+        return JSON.stringify(this.json);
+    }
+}
+
+type URIJson = {
+    type: string;
+    label: string;
+    uri: string;
+    altUri?: {
+        desktop: string;
+    }
+}
+
+export class URIAction {
+    json: URIJson;
+
+    constructor(label:string, uri: string) {
+        this.json = {
+            type: 'uri',
+            label: label,
+            uri: uri,
+        }
+    }
+
+    set altUriDesktop(uri: string) {
+        this.json.altUri = {desktop : uri};
+    }
+    
+    public dump(): string {
+        return JSON.stringify(this.json);
+    }
+}
+
+
+type CameraJson = {
+    type: string;
+    label: string;
+}
+
+export class CameraAction {
+    json: CameraJson;
+
+    constructor(label:string) {
+        this.json = {
+            type: 'camera',
+            label: label
+        }
+    }
+    
+    public dump(): string {
+        return JSON.stringify(this.json);
+    }
+}
+
+export class CameraRollAction {
+    json: CameraJson;
+
+    constructor(label:string) {
+        this.json = {
+            type: 'cameraRoll',
+            label: label
+        }
+    }
+    
+    public dump(): string {
+        return JSON.stringify(this.json);
+    }
+}
+
+type LocationJson = {
+    type: string;
+    label: string;
+}
+
+export class LocationAction {
+    json: LocationJson;
+
+    constructor(label:string) {
+        this.json = {
+            type: 'location',
+            label: label
+        }
+    }
+    
+    public dump(): string {
+        return JSON.stringify(this.json);
+    }
+}
--- test/action.test.ts
+++ test/action.test.ts
@@ -1,4 +1,4 @@
-import { PostbackAction } from '../src/action';
+import { PostbackAction, MessageAction, URIAction, CameraAction, CameraRollAction, LocationAction } from '../src/action';
 
 describe('test', () => {
     it ('postback action', () => {
@@ -12,5 +12,59 @@ describe('test', () => {
         +'"data":"action=buy&itemid=111",'
         +'"displayText":"Buy"'
         +'}');
-    })
+    });
+
+    it ('message action', () => {
+        const action = new MessageAction('Yes', 'Yes');
+        
+        const ret = action.dump();
+        expect(ret).toEqual('{' 
+        +'"type":"message",'
+        +'"label":"Yes",'
+        +'"text":"Yes"'
+        +'}');
+    });
+
+    it ('uri action', () => {
+        const action = new URIAction('View details', 'http://example.com/page/222');
+        action.altUriDesktop = 'http://example.com/pc/page/222';
+        
+        const ret = action.dump();
+        expect(ret).toEqual('{' 
+        +'"type":"uri",'
+        +'"label":"View details",'
+        +'"uri":"http://example.com/page/222",'
+        +'"altUri":{"desktop":"http://example.com/pc/page/222"}'
+        +'}');
+    });
+
+    it ('camera action', () => {
+        const action = new CameraAction('Camera');
+        
+        const ret = action.dump();
+        expect(ret).toEqual('{' 
+        +'"type":"camera",'
+        +'"label":"Camera"'
+        +'}');
+    });
+
+    it ('cameraRoll action', () => {
+        const action = new CameraRollAction('Camera roll');
+        
+        const ret = action.dump();
+        expect(ret).toEqual('{' 
+        +'"type":"cameraRoll",'
+        +'"label":"Camera roll"'
+        +'}');
+    });
+
+    it ('location action', () => {
+        const action = new LocationAction('Location');
+        
+        const ret = action.dump();
+        expect(ret).toEqual('{' 
+        +'"type":"location",'
+        +'"label":"Location"'
+        +'}');
+    });
 })
